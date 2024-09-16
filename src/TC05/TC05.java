c489962da2c37aecf85224ed50171260e8989ad2
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-09-16 17:07:30 +0300

    automation code for the user story 1st situation is done.

--- src/TC05/TC05.java
+++ src/TC05/TC05.java
@@ -1,11 +1,33 @@
 package TC05;
 
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class TC05 extends BaseDriver {
     @Test
-    public void Test(){
+    public void Test() {
+
+        driver.get("https://demowebshop.tricentis.com/");
+        driver.manage().window().maximize();
+        WebElement login = driver.findElement(By.xpath("//a[@href='/login']"));
+        login.click();
+        MyFunc.Bekle(2);
+        WebElement login2 = driver.findElement(By.xpath("//input[@value='Log in']"));
+        login2.click();
+        MyFunc.Bekle(2);
+        WebElement message = driver.findElement(By.cssSelector(".validation-summary-errors > span:nth-child(1)"));
+
+        Assert.assertTrue("Error", message.getText().contains("unsuccessful"));
+
+        BekleKapat();
+
 
     }
+
+
 }
