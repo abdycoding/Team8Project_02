da4ade5250259b1ad70904e9b00f85e218fd8d11
gul <kgulmeiram@gmail.com>
2024-09-18 20:10:38 +0300

    TC-07 oluşturuldu

--- src/TC07/TC07.java
+++ src/TC07/TC07.java
@@ -5,38 +5,38 @@ import Utility.MyFunc;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import static Utility.BaseDriver.driver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TC07 extends BaseDriver {
-        @Test
-        public void Test(){
-            driver.get("https://demowebshop.tricentis.com/");
+    @Test
+    public void Test(){
+        driver.get("https://demowebshop.tricentis.com/");
 
-            WebElement login= driver.findElement(By.xpath("//*[@class='ico-login']"));
-            login.click();
-            MyFunc.Bekle(2);
+        WebElement login= driver.findElement(By.xpath("//*[@class='ico-login']"));
+        login.click();
+        MyFunc.Bekle(2);
 
-            WebElement email= driver.findElement(By.xpath("//*[@class='email']"));
-            email.sendKeys("technostudyteam8@gmail.com");
-            MyFunc.Bekle(2);
+        WebElement email= driver.findElement(By.xpath("//*[@class='email']"));
+        email.sendKeys("technostudyteam8@gmail.com");
+        MyFunc.Bekle(2);
 
-            WebElement password= driver.findElement(By.xpath("//*[@class='password']"));
-            password.sendKeys("Ronaldo7");
-            MyFunc.Bekle(2);
+        WebElement password= driver.findElement(By.xpath("//*[@class='password']"));
+        password.sendKeys("Ronaldo7");
+        MyFunc.Bekle(2);
 
-            WebElement loginclk= driver.findElement(By.xpath("//*[@class='button-1 login-button']"));
-            loginclk.click();
-            MyFunc.Bekle(2);
+        WebElement loginclk= driver.findElement(By.xpath("//*[@class='button-1 login-button']"));
+        loginclk.click();
+        MyFunc.Bekle(2);
 
-            WebElement anket=driver.findElement(By.xpath("//*[@id='pollanswers-1']"));
-            anket.click();
-            MyFunc.Bekle(2);
+        WebElement anket=driver.findElement(By.xpath("//*[@id='pollanswers-1']"));
+        anket.click();
+        MyFunc.Bekle(2);
 
-            WebElement vote=driver.findElement(By.xpath("//*[@class='button-2 vote-poll-button']"));
-            vote.click();
-            MyFunc.Bekle(2);
-            BekleKapat();
-        }
+        WebElement vote=driver.findElement(By.xpath("//*[@class='button-2 vote-poll-button']"));
+        vote.click();
+        MyFunc.Bekle(2);
 
+        BekleKapat();
     }
+
+}
