25f0bab43a80045708991d94dbe4989670249bb4
furkanakalin <furkanakalin3423@gmail.com>
2024-09-19 10:45:40 +0300

    Part1 has been created

--- src/TC08/TC08.java
+++ src/TC08/TC08.java
@@ -1,4 +1,58 @@
 package TC08;
 
-public class TC08 {
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class TC08 extends BaseDriver {
+
+    @Test
+    public void Test(){
+
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement computers= driver.findElement(By.xpath("//*[@class='top-menu']//*[@href='/computers']"));
+        computers.click();
+
+        WebElement notebooks= driver.findElement(By.xpath("//*[@alt='Picture for category Notebooks']"));
+        wait.until(ExpectedConditions.elementToBeClickable(notebooks));
+        notebooks.click();
+
+        WebElement addtocard = driver.findElement(By.xpath("//*[@value='Add to cart']"));
+        addtocard.click();
+
+        WebElement shoppingcart = driver.findElement(By.xpath("//*[@class='ico-cart']//*[@class='cart-label']"));
+        shoppingcart.click();
+
+        WebElement applycoupon = driver.findElement(By.xpath("//*[@value='Apply coupon']"));
+        applycoupon.click();
+
+        WebElement message1 = driver.findElement(By.xpath("//*[@class='message']"));
+        message1.getText();
+
+        Assert.assertTrue("Bir hata oluştu", message1.isDisplayed());
+
+        WebElement addgiftcard = driver.findElement(By.xpath("//*[@value='Add gift card']"));
+        addgiftcard.click();
+
+        WebElement message2 = driver.findElement(By.xpath("//*[@class='message']"));
+        message2.getText();
+
+        Assert.assertTrue("Bir hata oluştu", message2.isDisplayed());
+
+        WebElement agreeterms = driver.findElement(By.xpath("//*[@class='totals']//*[@id='termsofservice']"));
+        agreeterms.click();
+
+        WebElement checkout = driver.findElement(By.xpath("//*[@id='checkout']"));
+        checkout.click();
+
+
+        BekleKapat();
+
+
+    }
+
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -2,25 +2,29 @@ package Utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BaseDriver {
 
     public static WebDriver driver;
+    public static WebDriverWait wait;
 
-     static{
-
-        driver=new ChromeDriver();
-
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    //bunun sarti extends olmasi ve basta yer almasi
+    static // tearStart()
+    {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühleti
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
     }
-    public static void BekleKapat(){
 
+
+    public static void BekleKapat() // tearDown()
+    {
         MyFunc.Bekle(3);
         driver.quit();
-
-
     }
 }
